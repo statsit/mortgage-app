86de83ccb602718faa30c2e5d57af1e899e7c8b4
Adebayo Akinlalu <aakinlalu@outlook.com>
2023-02-01 15:11:00 +1100

    updated

--- mortgage_app_js/mortgage_app.js
+++ mortgage_app_js/mortgage_app.js
@@ -11,35 +11,84 @@ class Connection {
     }
 }
 
-class Mortgage extends Connection {
-    constructor(host, 
-                dbName, 
-                user, 
-                password,
-                 port,
-                 lastMonth,
-                 principal,
-                 rate,
-                 payment) {
-        this.lastMonth = lastMonth;
+class MortgageSimulator {
+    constructor(principal, interestRate, startDate, monthlyPaymment, baseSaving, monthlyContribution) {
         this.principal = principal;
-        this.rate = rate;
-        this.payment = payment;
-        super(host, dbName, user, password, port);
+        this.interestRate = interestRate;
+        this.startDate = startDate;
+        this.monthlyPaymment = monthlyPaymment;
+        this.baseSaving = baseSaving;
+        this.monthlyContribution = monthlyContribution;
+        
+
     }
-    
-    setMonthPayment(date, amount) {
-        if (this.getBalance() > 0) {
-             this.principal = this.getBalance()
-             
+
+    addDays(date) {
+        date.setDate(date.getDate() + 30);
+        return date;
+    }
+
+    calculate() {
+        let recPrincipal = [];
+        let month = this.startDate.getMonth();
+        let year = this.startDate.getFullYear();
+
+        while (this.principal > 0) {
+            let interest = (this.principal * this.interestRate)/12;
+            this.principal = (this.principal + interest) - this.monthlyPaymment
+            this.baseSaving += this.monthlyContribution
+            // recPrincipal.push(this.principal);
+            // date.push(this.startDate);
+
+            recPrincipal.push(
+                {
+                    date: this.startDate, 
+                    principal: this.principal,
+                    saving: this.baseSaving
+                }
+                    )
+            if (month === 11) {
+                month = 0;
+                year += 1;
+                this.startDate = new Date(year, month, 2);
+            }
+            else {
+                month += 1;
+                this.startDate = new Date(year, month, 2);
+            }
         }
-    }
-    getBalance() {
-        return 0
+        // console.log(recPrincipal.length, recPrincipal.length/12)
 
+        return recPrincipal;
     }
-    insertDbResult(result) {
+};
 
-    }
-    
-}
+let principal = document.getElementById('principal').value;
+let interestRate = document.getElementById('interestRate').value;
+let startDate = document.getElementById('date').value;
+let baseSaving = document.getElementById('basesaving').value;
+
+
+let m = new MortgageSimulator(48000000, 0.15, new Date(2023, 3, 2), 1500000, 55000,2000);
+let res = m.calculate();
+
+const table = document.createElement('table');
+const header = document.createElement('tr');
+header.innerHTML = `<th>Date</th><th>Principal</th><th>Saving</th>`
+table.appendChild(header);
+
+res.forEach((item) => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td>${item.date.toLocaleDateString()}</td>
+    <td>$${item.principal.toFixed(2)}</td>
+    <td>$${item.saving}.00</td>
+    `
+    table.appendChild(row);
+})
+
+document.body.appendChild(table);
+
+// console.table(m.calculate())
+
+console.log(principal)
