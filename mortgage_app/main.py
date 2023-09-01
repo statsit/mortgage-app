b1438faeaa7daf3357bbd47d485b8e19f96311dc
Adebayo Akinlalu <aakinlalu@outlook.com>
2023-09-01 15:19:44 +1000

    add cors to main.py

--- mortgage_app/main.py
+++ mortgage_app/main.py
@@ -1,5 +1,6 @@
 import asyncio
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from mortgage_app.api import mortgages
 from mortgage_app.db import database, engine, metadata
@@ -7,6 +8,16 @@ from mortgage_app.db import database, engine, metadata
 metadata.create_all(engine)
 
 app = FastAPI()
+
+origins = ["http://localhost:3000", "http://10.0.0.103:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True, 
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup():
